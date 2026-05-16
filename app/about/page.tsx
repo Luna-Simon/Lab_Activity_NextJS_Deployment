9306ca77bc1ff9a1eba416f46d6a07c4aecb7bdd
Luna-Simon <simon.labay@ciit.edu.ph>
2026-05-16 21:15:05 +0800

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+    const newLocal = <h1>About Page</h1>;
+  return (
+    <div>
+      {newLocal}
+      <p>This page is all about our Next.js project.</p>
+    </div>
+  );
+}
